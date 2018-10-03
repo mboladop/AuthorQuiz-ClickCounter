ccffcdb2c400157e2c9a4aaaa031be7fab5e0f18
mboladop <mboladop@gmail.com>
2018-10-03 12:25:40 +0100

    non-interactive parts of app: title + instructions + initial structure for game elements + continue button

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import './bootstrap.min.css';
 
+function Hero(){
+  return (<div className="row">
+    <div class="jumbotron col-10 offset-1">
+      <h1>Author Quiz </h1>
+      <p>Select the book written by the author shown :)</p>
+    </div>
+  </div>);
+}
+
+function Turn(){
+  return (<div/>);
+}
+function Continue(){
+  return (<div/>);
+}
+
 class AuthorQuiz extends Component {
   render() {
     return (
-      <div>Author Quiz</div>
+      <div className="container-fluid">
+      <Hero/>
+      <Turn/>
+      <Continue/>
+      </div>
     );
   }
 }
