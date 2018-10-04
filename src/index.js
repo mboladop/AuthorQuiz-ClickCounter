d87ee0fe2df9279fb14bf0a5f83bb0a96b4fe7c9
mboladop <mboladop@gmail.com>
2018-10-04 12:40:54 +0100

    non-interactive parts: new authors added

--- src/index.js
+++ src/index.js
@@ -3,21 +3,91 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
+import {shuffle, sample} from 'underscore';
+
 
 const authors=[
     {
         name:'Mark Twain',
         imageUrl:'images/authors/marktwain.jpg',
         imageSource:'Wkimedia Commons',
-        books: ['The Adventures of Huckleberry Finn']
+        books: [
+            'The Adventures of Huckleberry Finn',
+            'Life on the Mississippi',
+            'Roughing'
+        ]
+    },
+    
+    {
+        name:'JK Rowling',
+        imageUrl:'images/authors/jk.jpg',
+        imageSource:'Wkimedia Commons',
+        books: [
+            'Harry Potter and the Philosophers Stone',
+            'The Tales of Beedle the Bard',
+            'Fantastic Beasts and Where to Find Them'
+        ]
+    },
+
+    {
+        name:'William Shakespeare',
+        imageUrl:'images/authors/ws.jpg',
+        imageSource:'Wkimedia Commons',
+        books: [
+            'Romeo and Juliette',
+            'Macbeth',
+            'The Merry Wives of Windsor'
+        ]
+    },
+    {
+        name:'Gloria Fuertes',
+        imageUrl:'images/authors/gf.jpg',
+        imageSource:'Wkimedia Commons',
+        books: [
+            'Songs for kids',
+            'The months',
+            'Ponytail the poet'
+        ]
+    },
+    {
+        name:'Ernest Hemingway',
+        imageUrl:'images/authors/eh.jpg',
+        imageSource:'Wkimedia Commons',
+        books: [
+            'The Torrents of Spring',
+            'Islands in the Stream',
+            'The Garden of Eden'
+        ]
+    },
+    {
+        name:'León Tolstói',
+        imageUrl:'images/authors/lt.jpg',
+        imageSource:'Wkimedia Commons',
+        books: [
+            'War & Peace',
+            'Anna Karenina',
+            'Resurrection'
+        ]
     }
+    
 ];
+function getTurnData(authors){
+    const allBooks = authors.reduce(function (p,c,i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => 
+                author.books.some ((title) => 
+                    title === answer)),
+    }
+}
 
 const state={
-    turnData: {
-    author: authors[0],
-    books: authors[0].books
-    }
+    turnData: getTurnData(authors)
 };
 
 
