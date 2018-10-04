cac05661624a883593a4009c52210636ea071962
mboladop <mboladop@gmail.com>
2018-10-04 19:17:03 +0100

    add of enzyme and enzyme adapter for tests

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
+import Enzyme , {mount, shallow, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter() });
+
+
+
 
 describe ("Author Quiz", ()=>{
   it('renders without crashing', () => {
