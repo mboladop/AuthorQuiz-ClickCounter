eceb3d3fb451e89d175fa7a8557a37848e297c89
mboladop <mboladop@gmail.com>
2018-10-09 20:03:04 +0100

    link add to function AuthorQuiz to add new author route using Link from react-router-dom

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './App.css';
 import './bootstrap.min.css';
@@ -73,6 +74,7 @@ function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
       <Hero/>
       <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
       <Continue/>
+      <p><Link to='/add'>Add an Author</Link></p>
       <Footer/>
       </div>
     ); 
