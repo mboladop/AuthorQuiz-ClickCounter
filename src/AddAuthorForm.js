4a152b295195e71d58d5b3c5babe7646f867d958
mboladop <mboladop@gmail.com>
2018-10-10 12:26:03 +0100

    Adding forms: AddAuthorform.js

---
+++ src/AddAuthorForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function AddAuthorForm(match) {
+    return <div>
+        <h1>Add Author</h1>
+        <p>{JSON.stringify(match)}</p>
+    </div>;
+}
+
+export default AddAuthorForm;
