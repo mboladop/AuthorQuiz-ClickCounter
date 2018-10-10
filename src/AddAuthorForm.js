7041387c6e2281ec6c5119b7bbadfa14d049a13e
mboladop <mboladop@gmail.com>
2018-10-10 12:58:53 +0100

    Adding forms: two empty props created in AuthorForm class to associate via state to both inputs and making them changeable by the user

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -2,15 +2,22 @@ import React from 'react';
 import'./AddAuthorForm.css';
 
 class AuthorForm extends React.Component {
+    constructor (props){
+        super(props);
+        this.state = {
+            name: '',
+            imageUrl: ''
+        };
+    }
     render () {
        return <form>
             <div className='AddAuthorForm_input'>
                 <label htmlFor='name'>Name</label>
-                <input type='text' name='name'/>
+                <input type='text' name='name' value={this.state.name}/>
             </div>
             <div className='AddAuthorForm_input'>
                 <label htmlFor='imageUrl'>Image Url</label>
-                <input type='text' name='imageUrl'/>
+                <input type='text' name='imageUrl' value={this.state.imageUrl}/>
             </div>
         </form>
     }
